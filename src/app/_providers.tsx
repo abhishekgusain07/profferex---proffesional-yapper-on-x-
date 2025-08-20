46fe6c719445e8d36d6f17be595fae92051d135b
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-08-20 17:32:23 +0530

    fix: post and schedule buttons

--- src/app/_providers.tsx
+++ src/app/_providers.tsx
@@ -4,6 +4,7 @@ import { TRPCProvider } from '@/trpc/client'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { AccountProvider } from '@/hooks/use-account'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { Toaster } from 'react-hot-toast'
 
 interface ProvidersProps {
   children: React.ReactNode
@@ -22,6 +23,16 @@ export function Providers({ children, initialSession }: ProvidersProps) {
       <TRPCProvider initialSession={initialSession}>
         <AccountProvider>
           {children}
+          <Toaster 
+            position="top-right"
+            toastOptions={{
+              duration: 4000,
+              style: {
+                background: '#363636',
+                color: '#fff',
+              },
+            }}
+          />
           {/* Only show DevTools in development */}
           {process.env.NODE_ENV === 'development' && (
             <ReactQueryDevtools initialIsOpen={false} />
